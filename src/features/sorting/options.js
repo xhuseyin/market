fd08645863f284bf7b2baecbb1d0bfa7d182dda2
Hüseyin Çakmak <xhuseyincakmak@gmail.com>
2022-09-20 17:28:33 +0300

    Create sorting feature

---
+++ src/features/sorting/options.js
@@ -0,0 +1,20 @@
+const sortingOptions = [
+  {
+    id: "lowToHigh",
+    value: "Price low to high",
+  },
+  {
+    id: "highToLow",
+    value: "Price high to low",
+  },
+  {
+    id: "newToOld",
+    value: "New to old",
+  },
+  {
+    id: "oldToNew",
+    value: "Old to new",
+  },
+];
+
+export default sortingOptions;
