cec8e8eb24dc419d002fd7215bfdd1b725fdcfab
Hüseyin Çakmak <xhuseyincakmak@gmail.com>
2022-09-21 10:58:34 +0300

    Create products feature

---
+++ src/components/productPager/index.jsx
@@ -0,0 +1,143 @@
+import React from "react";
+import styled from "styled-components";
+import colors from "../../theme/foundations/colors";
+
+const Pagination = styled.div`
+  width: inherit;
+  margin-top: 32px;
+  margin-bottom: 136px;
+  height: 40px;
+  border-radius: 2px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const PageItem = styled.div`
+  width: 62px;
+  height: 24px;
+  display: flex;
+  justify-content: space-between;
+  cursor: pointer;
+`;
+const PageItemLabel = styled.div`
+  width: 31px;
+  height: 24px;
+  line-height: 24px;
+  font-size: 14px;
+  font-weight: 600;
+  text-align: center;
+`;
+
+const DirectionIcon = styled.div`
+  width: 24px;
+  height: 24px;
+  background-size: cover;
+`;
+const LeftIcon = styled(DirectionIcon)`
+  background-image: url("../../assets/icons/left-arrow.png");
+
+  ${PageItemLabel} {
+    color: ${colors.blue};
+  }
+`;
+const RightIcon = styled(DirectionIcon)`
+  background-image: url("../../assets/icons/right-arrow.png");
+
+  ${PageItemLabel} {
+    color: ${colors.gray};
+  }
+`;
+
+const Container = styled.div`
+  width: auto;
+  height: 40px;
+  border-radius: 2px;
+  display: flex;
+  margin-left: 50px;
+  margin-right: 50px;
+`;
+
+const Content = styled.div`
+  width: 32px;
+  height: 40px;
+  line-height: 40px;
+  font-size: 14px;
+  font-weight: 600;
+  text-align: center;
+  cursor: pointer;
+  color: ${colors.gray};
+
+  &[status="active"]{
+    background-color: ${colors.blue};
+    color: ${colors.white};
+    border-radius: 2px;
+  }
+`;
+const ProductPager = ({ activePage, onChange, dataCount, itemCount }) => {
+  const pageCount = parseInt(dataCount / itemCount) + 1;
+
+  return (
+    <Pagination>
+      <PageItem onClick={() => onChange(activePage === 1 ? 1 : activePage - 1)}>
+        <LeftIcon />
+        <PageItemLabel>Prev</PageItemLabel>
+      </PageItem>
+
+      <Container>
+        {pageCount <= 9 && (
+          <>
+            {[...Array(pageCount)].map((x, i) => (
+              <React.Fragment key={i}>
+                <Content
+                  onClick={() => onChange(i + 1)}
+                  status={i + 1 === activePage ? "active" : "passive"}
+                >
+                  {i + 1}
+                </Content>
+              </React.Fragment>
+            ))}
+          </>
+        )}
+        {pageCount > 9 && (
+          <>
+            {[...Array(pageCount)].map((x, i) => (
+              <React.Fragment key={i}>
+                {i + 1 <= 4 && (
+                  <Content
+                    onClick={() => onChange(i + 1)}
+                    status={i + 1 === activePage ? "active" : "passive"}
+                  >
+                    {i + 1}
+                  </Content>
+                )}
+              </React.Fragment>
+            ))}
+            <Content>...</Content>
+            {[...Array(pageCount)].map((x, i) => (
+              <React.Fragment key={i}>
+                {pageCount - 4 < i + 1 && (
+                  <Content
+                    onClick={() => onChange(i + 1)}
+                    status={i + 1 === activePage ? "active" : "passive"}
+                  >
+                    {i + 1}
+                  </Content>
+                )}
+              </React.Fragment>
+            ))}
+          </>
+        )}
+      </Container>
+      <PageItem
+        onClick={() =>
+          onChange(activePage === pageCount ? pageCount : activePage + 1)
+        }
+      >
+        <PageItemLabel>Next</PageItemLabel>
+        <RightIcon />
+      </PageItem>
+    </Pagination>
+  );
+};
+
+export default ProductPager;
---
+++ src/features/products/index.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { addItem } from "../../store/actions/basket";
+
+// Components
+import Title from "../../components/boxTitle";
+import Grid from "../../components/grid";
+import ProductFilter from "../../components/productFilter";
+import ProductPager from "../../components/productPager";
+import ProductTile from "../../components/productTile";
+import EmptyResult from "../../components/emptyResult";
+
+const Products = ({
+  data,
+  relatedTypes,
+  selectedTypes,
+  selectType,
+  itemCount,
+}) => {
+  //const dispatch = useDispatch();
+  const [rangeStart, setRangeStart] = useState(null);
+  const [rangeEnd, setRangeEnd] = useState(null);
+  const [activePage, setActivePage] = useState(1);
+
+  useEffect(() => {
+    setRangeStart(activePage * itemCount - itemCount);
+    setRangeEnd(activePage * itemCount);
+  }, [activePage, itemCount]);
+
+  return (
+    <>
+      <Title size="md" title="Products" />
+      {data.length > 0 ? (
+        <>
+          <ProductFilter
+            relatedTypes={relatedTypes}
+            selectedTypes={selectedTypes}
+            selectType={selectType}
+          />
+          <Grid>
+            {data.map((item, index) => (
+              <React.Fragment key={index}>
+                {index >= rangeStart && index + 1 <= rangeEnd && (
+                  <ProductTile
+                    label={item.name}
+                    price={item.price}
+                    onClick={() => dispatch(addItem(item))}
+                  />
+                )}
+              </React.Fragment>
+            ))}
+          </Grid>
+          <ProductPager
+            dataCount={data.length}
+            itemCount={16}
+            activePage={activePage}
+            onChange={(val) => setActivePage(val)}
+          />
+        </>
+      ) : (
+        <EmptyResult label="No results found" />
+      )}
+    </>
+  );
+};
+
+export default Products;
